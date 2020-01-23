a93c161433233bc28ba8603b7edc40a050647499
Thiago de Bastos <thiago@thiagodebastos.com>
2020-01-24 03:23:27 +1100

    feature(security): add basic CORS support for dev env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const { contactsEndpointHandler } = require("./contacts");
 const { adaptRequest } = require("./helpers/adapt-request");
@@ -7,21 +8,25 @@ const { config } = require("./config");
 
 require("dotenv").config();
 
-const app = express();
 const PORT = config.port;
+
+const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3001" })); // TODO: set origin depending on process.env.NODE_ENV
 app.use(helmet());
 
 // routes
 app.all("/contacts", contactsController);
 app.get("/contacts/:id", contactsController);
+app.delete("/contacts/:id", contactsController);
 
 // controllers
 /**
- *
+ * Contacts Controller
+ * Keep business logic out of here
  * @param {import("@types/express").Request} req
  * @param {import("@types/express").Response} res
  */
